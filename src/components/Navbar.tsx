e4348bb1292772cef00c05ed4bde52a8ceb927cb
hadi <kurbansapi179@gmail.com>
2026-06-29 17:17:22 +0700

    Update narrative to remove internal and add cache busting for logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
       <Link href="/" className="flex items-center gap-2.5 font-bold text-lg text-white no-underline">
         <div className="relative w-8 h-8 rounded-lg overflow-hidden border border-black bg-white shrink-0">
           <Image
-            src="/logo.png"
+            src="/logo.png?v=2"
             alt="TOYO SENSING PONG Logo"
             fill
             sizes="32px"
